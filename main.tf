f383178f3201ba51153025af6eebd6a71ae7a46d
Nicole K <dketchiozo@gmail.com>
2024-07-02 13:44:57 -0400

    added vm resource configuration

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
+## Initialises Terraform with Azure Provider
 terraform {
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -12,7 +12,69 @@ provider "azurerm" {
   features {}
 }
 
+## Defines the Virtual Machine configurations
+variable "prefix" {
+  default = "tfvmlearning"
+}
+
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-terraform-vm"
-    location = "eastus"
+  name     = "${var.prefix}-rg-terraform-vm"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "dev"
+  }
 }
